47c57502da860f77e4b733ce8ee09e246f5b8dd7
saibat01 <mohammedmehdisaibat@gmail.com>
2025-05-05 15:22:37 +0100

    feat: implement getComputerChoice function to generate computer's move

--- script.js
+++ script.js
@@ -1,1 +1,7 @@
 "use strict";
+
+function getComputerChoice() {
+  const choices = ["rock", "paper", "scissors"];
+  const randomIndex = Math.floor(Math.random() * choices.length);
+  return choices[randomIndex];
+}
